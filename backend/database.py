ffef85538af167f5eee2835fcae5e4791f045fbd
Your Name <you@example.com>
2026-06-29 11:25:16 +0300

    финальная структура проекта.

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:lala@localhost:5434/spbtech_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://my_project_db_4dc4_user:xcDvLuYMTAQ4mpEhItgozgwa2Y2bweDb@dpg-d912j5pkh4rs739s7p20-a.ohio-postgres.render.com/my_project_db_4dc4"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
